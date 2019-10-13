7c643482dccf90a04b71f6d186365451347ebf14
Justin Zhang <zjustin@umich.edu>
2019-10-12 22:18:30 -0400

    loading screen first draft

--- client/screens/MainScreen.js
+++ client/screens/MainScreen.js
@@ -1,34 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   View,
   SafeAreaView,
   TextInput,
   Text,
-  Alert,
-} from 'react-native';
-import { Button } from 'react-native-elements';
-import SocketContext from '../socket-context';
-import * as Permissions from 'expo-permissions';
-import RaceScreen from './RaceScreen';
+  Alert
+} from "react-native";
+import { Button } from "react-native-elements";
+import SocketContext from "../socket-context";
+import * as Permissions from "expo-permissions";
+import RaceScreen from "./RaceScreen";
 
 class MainScreen extends Component {
   constructor(props) {
-    super(props)
-    this.state = { button_state: "Find Match!",
-    count: 0,
-    username: '',
-    password: '',
-    saved_username: ''};
+    super(props);
+    this.state = {
+      button_state: "Find Match!",
+      count: 0,
+      username: "",
+      password: "",
+      saved_username: ""
+    };
     this.pressFindMatch = this.pressFindMatch.bind(this);
     this.saveLogin = this.saveLogin.bind(this);
-    this.onReceivedGameConfirmation = this.onReceivedGameConfirmation.bind(this);
+    this.onReceivedGameConfirmation = this.onReceivedGameConfirmation.bind(
+      this
+    );
   }
 
   _getLocationPermission = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      Alert.alert('Run Royale needs location services to track run progress. Please change permissions in the Settings app.');
+    if (status !== "granted") {
+      Alert.alert(
+        "Run Royale needs location services to track run progress. Please change permissions in the Settings app."
+      );
     }
   };
 
@@ -39,133 +45,133 @@ class MainScreen extends Component {
 
   pressFindMatch(username) {
     if (this.state.login_button_disable) {
-      this.setState({button_state: "Looking for match..."});
-      this.props.socket.emit('find-game-event', username);
-      this.props.socket.on('game-found-event', this.onReceivedGameConfirmation);
+      this.setState({ button_state: "Looking for match..." });
+      this.props.socket.emit("find-game-event", username);
+      this.props.socket.on("game-found-event", this.onReceivedGameConfirmation);
     }
   }
 
   saveLogin() {
-    if (this.state.username != '') {
-      this.setState({saved_username: this.state.username});
-      this.setState({login_button_state: "Logged in!"});
-      this.setState({login_button_disable: true});
+    if (this.state.username != "") {
+      this.setState({ saved_username: this.state.username });
+      this.setState({ login_button_state: "Logged in!" });
+      this.setState({ login_button_disable: true });
     }
   }
 
   // Game found handler
   onReceivedGameConfirmation() {
-    Alert.alert('Game found!');
-    this.props.navigation.navigate('Links');
+    Alert.alert("Game found!");
+    this.props.navigation.navigate("Loading");
   }
 
   render() {
-    const login_message = <Text style={styles.user_tag}>Please login.</Text>
-    const hello_message = <Text style={styles.user_tag}>Hello, {this.state.saved_username}!</Text>
+    const login_message = <Text style={styles.user_tag}>Please login.</Text>;
+    const hello_message = (
+      <Text style={styles.user_tag}>Hello, {this.state.saved_username}!</Text>
+    );
     return (
-        <SafeAreaView style={styles.container}>
-            <View>
-                <Text style={styles.title}>Run Royale</Text>
-            </View>
-            <View style={styles.container}>
-                <TextInput
-                    value={this.state.username}
-                    onChangeText={(username) => this.setState({ username })}
-                    placeholder={'Username'}
-                    style={styles.input}
-                />
-                <TextInput
-                    value={this.state.password}
-                    onChangeText={(password) => this.setState({ password })}
-                    placeholder={'Password'}
-                    secureTextEntry={true}
-                    style={styles.input}
-                />
-                <Button
-                    raised
-                    disabled={this.state.login_button_disable}
-                    type="solid"
-                    title={this.state.login_button_state}
-                    onPress={this.saveLogin}
-                />
-            </View>
-            <View style={styles.username_display}>
-                {this.state.saved_username == '' ? login_message : hello_message}
-            </View>
-            <View style={styles.buttonContainer}>
-                <Button
-                raised
-                type="solid"
-                title= {this.state.button_state}
-                onPress={() => this.pressFindMatch(this.state.saved_username)}
-            />
-            </View>
-        </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.title}>Run Royale</Text>
+        </View>
+        <View style={styles.container}>
+          <TextInput
+            value={this.state.username}
+            onChangeText={username => this.setState({ username })}
+            placeholder={"Username"}
+            style={styles.input}
+          />
+          <TextInput
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            placeholder={"Password"}
+            secureTextEntry={true}
+            style={styles.input}
+          />
+          <Button
+            raised
+            disabled={this.state.login_button_disable}
+            type="solid"
+            title={this.state.login_button_state}
+            onPress={this.saveLogin}
+          />
+        </View>
+        <View style={styles.username_display}>
+          {this.state.saved_username == "" ? login_message : hello_message}
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            raised
+            type="solid"
+            title={this.state.button_state}
+            onPress={() => this.pressFindMatch(this.state.saved_username)}
+          />
+        </View>
+      </SafeAreaView>
     );
   }
 }
 
-const MainScreenWithSocket = (props) => (
+const MainScreenWithSocket = props => (
   <SocketContext.Consumer>
     {socket => <MainScreen {...props} socket={socket} />}
   </SocketContext.Consumer>
-)
+);
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 5,
-      marginHorizontal: 16,
-    },
-    username_display: {
-      textAlign: 'center',
-      textAlignVertical: 'center',
-    },
-    user_tag: {
-        fontFamily: "Helvetica",
-        color: 'blue',
-        fontWeight: 'bold',
-        fontSize: 30,
-        textAlign: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-      fontFamily: "Helvetica-Bold",
-      color: 'blue',
-      fontWeight: 'bold',
-      fontSize: 30,
-      textAlign: 'center',
-      textDecorationLine: 'underline',
-    },
-    fixToText: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    buttonContainer: {
-        width: '100%',
-        height: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        bottom: 60
-    },
-    separator: {
-      marginVertical: 8,
-      borderBottomColor: '#737373',
-    },
-    signin_form: {
-        flex: 1,
-        justifyContent: "center",
-        width: "80%"
-      },
-    input: {
-        width: 200,
-        height: 44,
-        padding: 10,
-        borderWidth: 1,
-        borderColor: 'transparent',
-        marginBottom: 10,
-    },
-  });
+  container: {
+    flex: 1,
+    marginTop: 5,
+    marginHorizontal: 16
+  },
+  username_display: {
+    textAlign: "center",
+    textAlignVertical: "center"
+  },
+  user_tag: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30,
+    textAlign: "center",
+    justifyContent: "center"
+  },
+  title: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30,
+    textAlign: "center",
+    textDecorationLine: "underline"
+  },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  buttonContainer: {
+    width: "100%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 60
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: "#737373"
+  },
+  signin_form: {
+    flex: 1,
+    justifyContent: "center",
+    width: "80%"
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "transparent",
+    marginBottom: 10
+  }
+});
 
-export default MainScreenWithSocket
+export default MainScreenWithSocket;
